1ac456615998ab57d86cb1247614ac3a99199269
Kirill Guryev <k.guryev@btc-s.ru>
2021-09-16 15:02:51 +0300

    add LoyoutComponent

---
+++ src/views/LoyoutFireBase.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app-wrapper">
+    <div class="app">
+      <navigaton />
+      <router-view />
+      <Footer />
+    </div>
+  </div>
+</template>
+<script>
+import Footer from '../components/Footer.vue'
+import Navigaton from '../components/Navigaton.vue'
+export default {
+  components: {
+    Footer,
+    Navigaton
+  }
+}
+</script>
